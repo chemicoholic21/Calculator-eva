78a5384e79da5df5d8edb05316cc48f67cfa55e6
Taniya <97229694+chemicoholic21@users.noreply.github.com>
2023-10-02 11:13:46 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+const buttons = document.querySelectorAll('button')
+const screenDisplay = document.querySelector('.screen')
+
+let calculation = []
+let accumulativeCalculation
+
+console.log(buttons)
+
+function calculate(button){
+    const value = button.textContent
+    if(value == "CLEAR"){
+        calculation = []
+        screenDisplay.textContent = '0'
+    }else if(value === "="){
+        console.log(accumulativeCalculation)
+        screenDisplay.textContent = eval(accumulativeCalculation)
+    } else {
+        calculation.push(value)
+        accumulativeCalculation = calculation.join('')
+        screenDisplay.textContent = accumulativeCalculation 
+    }
+}
+
+buttons.forEach(button=>button.addEventListener('click',))
+
